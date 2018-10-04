26bcf512cca682d6ac4483f6f54aa7d9dc1f3818
maganer.krik <maganer.krik@mail.ru>
2018-10-04 21:43:36 +0200

    made checking of work regime entering and improved name generating

--- fsng.py
+++ fsng.py
@@ -3,6 +3,12 @@ from random import *
 
 regime_of_work = input("say ass or boobs?") # :D
 
+while regime_of_work != "ass" or regime_of_work != "boobs":
+    regime_of_work = input("say ass or boobs?")
+    if regime_of_work == "ass" or regime_of_work == "boobs":
+        break
+    
+
 number_of_names = 0 # that will be used to create new world
 names = []     # contains all names
 names_to_use = [] # this list contains names that will be used
@@ -41,17 +47,26 @@ if regime_of_work == "ass":
     for word in names:
         names_to_use.append(word)
 
-
 for word in names_to_use:
     processed_name = ""
+
     # it means get position of start char in word
     # also script does to find end word char
+
     start_word_char = randint(0, len(word))
     end_word_char = randint(start_word_char, len(word))
     
+    if start_word_char == 0:
+        start_word_char = randint(0, len(word))
+    if end_word_char == 0:
+        end_word_char = randint(start_word_char, len(word))
+
     # set word
     i = start_word_char
     y = end_word_char
+    if i > y:
+        i = end_word_char
+        y = start_word_char
     while i != y:
         processed_name+=word[i]
         i+=1
